f75c31013995e5076602f4aff75184bbf9629026
anshi1216 <anshidj123@gmail.com>
2025-09-03 16:04:25 +0530

    Initial Nx Workspace commit

---
+++ ticketingWorkspace/apps/apiApplication-e2e/jest.config.ts
@@ -0,0 +1,18 @@
+export default {
+  displayName: 'apiApplication-e2e',
+  preset: '../../jest.preset.js',
+  globalSetup: '<rootDir>/src/support/global-setup.ts',
+  globalTeardown: '<rootDir>/src/support/global-teardown.ts',
+  setupFiles: ['<rootDir>/src/support/test-setup.ts'],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apiApplication-e2e',
+};
