2b422703eab3253422721ae2612b08d7181ddc86
anshi1216 <anshidj123@gmail.com>
2025-09-04 10:14:45 +0530

    Angular: added book ticket and booking history page

--- ticketingWorkspace/apps/bookingApplication/src/app/app.ts
+++ ticketingWorkspace/apps/bookingApplication/src/app/app.ts
@@ -1,13 +1,46 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcome } from './nx-welcome';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [RouterModule, FormsModule, CommonModule],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected title = 'bookingApplication';
+  booking = { name: '', event: '', date: '' };
+  successMessage = '';
+  bookings: any[] = [];
+  apiUrl = 'http://localhost:3000/bookings'; 
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getBookingHistory();
+  }
+
+  onSubmit() {
+    this.http.post(this.apiUrl, this.booking).subscribe({
+      next: () => {
+        this.successMessage = 'Ticket booked successfully!';
+        this.booking = { name: '', event: '', date: '' };
+        this.getBookingHistory();
+      },
+      error: () => {
+        this.successMessage = 'Booking failed. Please try again.';
+      }
+    });
+  }
+
+  getBookingHistory() {
+    this.http.get<any[]>(this.apiUrl).subscribe({
+      next: (data) => this.bookings = data,
+      error: () => this.bookings = []
+    });
+  }
 }
---
+++ ticketingWorkspace/libs/utlilities/constants.ts
@@ -0,0 +1,6 @@
+export const TEXT = {
+  appTitle: 'My Shared App',
+  headerLabel: 'Welcome to the Shared Header!',
+  footerLabel: '© 2024 My Company',
+  buttonLabel: 'Click Me',
+};
