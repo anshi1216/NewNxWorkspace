99415489c1f9ed3d5c24ad792e7a3cf54ab6fe04
anshi1216 <anshidj123@gmail.com>
2025-09-04 14:56:37 +0530

    css changes

--- ticketingWorkspace/libs/reactLib/src/lib/footer/footer.tsx
+++ ticketingWorkspace/libs/reactLib/src/lib/footer/footer.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { TEXT } from '@ticketing-workspace/shared-utilities';
+import styles from './footer.module.scss';
 
 export const Footer: React.FC = () => {
   return (
-    <footer>
+    <footer className= {styles.footer}>
       <span>{TEXT.footerLabel}</span>
     </footer>
   );
--- ticketingWorkspace/libs/reactLib/src/lib/header/header.tsx
+++ ticketingWorkspace/libs/reactLib/src/lib/header/header.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { TEXT } from '@ticketing-workspace/shared-utilities';
+import styles from './header.module.scss';
 
 export const Header: React.FC = () => {
   return (
-    <header>
+    <header className={styles.header}>       
       <h1>{TEXT.appTitle}</h1>
       <span>{TEXT.headerLabel}</span>
       <button>{TEXT.buttonLabel}</button>
